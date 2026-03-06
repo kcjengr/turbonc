c99c954ab795dc69867ddae31a4c25f98d4c1b3c
TurBoss <turboss@linuxcnc25.lan>
2026-03-06 05:28:34 +0100

    enable Light/Dark theme

--- src/tnc/main.py
+++ src/tnc/main.py
@@ -6,7 +6,8 @@ import os
 os.environ['QT_API'] = 'pyside6'
 
 from PySide6.QtCore import Slot
-from PySide6.QtWidgets import QAbstractButton
+from PySide6.QtGui import QPalette
+from PySide6.QtWidgets import (QAbstractButton, QApplication)
 
 from qtpyvcp.widgets.form_widgets.main_window import VCPMainWindow
 
@@ -21,15 +22,58 @@ import resources_rc
 
 VCP_DIR = os.path.dirname(os.path.abspath(__file__))
 
+LIGHT_STYLESHEET_FILE = "light.qss"
+DARK_STYLESHEET_FILE = "dark.qss"
+
 
 class MainWindow(VCPMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
+        self._connect_theme_tracking()
+        self._apply_theme_stylesheet()
+        
         self.initUi()
 
         # self.plot.camera.Zoom(0.002)
         # self.plot.setViewMachine()
+
+    def _is_dark_theme(self):
+        app = QApplication.instance()
+        if app is None:
+            return False
+        palette = app.palette()
+        window_lightness = palette.color(QPalette.Window).lightness()
+        base_lightness = palette.color(QPalette.Base).lightness()
+        return ((window_lightness + base_lightness) / 2) < 128
+
+    def _connect_theme_tracking(self):
+        app = QApplication.instance()
+        if app is None:
+            return
+        palette_changed = getattr(app, 'paletteChanged', None)
+        if palette_changed is not None:
+            try:
+                palette_changed.connect(self._on_palette_changed)
+            except Exception:
+                LOG.exception("Failed to connect paletteChanged signal")
+
+    def _on_palette_changed(self, *_args):
+        QTimer.singleShot(0, self._apply_theme_stylesheet)
+
+    def _apply_theme_stylesheet(self):
+        dark_theme = self._is_dark_theme()
+        stylesheet_file = DARK_STYLESHEET_FILE if dark_theme else LIGHT_STYLESHEET_FILE
+        stylesheet_path = os.path.join(VCP_DIR, stylesheet_file)
+        app = QApplication.instance()
+        if app is None:
+            return
+        try:
+            with open(stylesheet_path, 'r', encoding='utf-8') as style_file:
+                app.setStyleSheet(style_file.read())
+        except Exception:
+            LOG.exception("Failed to load theme stylesheet: %s", stylesheet_path)
+
 
     @Slot(QAbstractButton)
     def on_probeTabGroup_buttonClicked(self, button):
