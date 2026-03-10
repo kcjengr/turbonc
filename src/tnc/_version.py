2a17f2d1ef082d624bb2615cedefbf6bae26e331
Chris Polanski <small.shop.concepts@gmail.com>
2026-03-10 11:51:51 -0400

    fix for warning in tnc

--- src/tnc/_version.py
+++ src/tnc/_version.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.4.3"
-__version_tuple__ = (0, 4, 3)
+__version__ = "0.5.1.post2.dev0+bbf6fbe"
+__version_tuple__ = (0, 5, 1, "post2", "dev0", "bbf6fbe")
