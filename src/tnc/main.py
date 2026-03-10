bbf6fbe49fdc53d200301907edf83a3a074295d2
Chris Polanski <small.shop.concepts@gmail.com>
2026-03-10 11:29:06 -0400

    display changes for --develop option in ini file, one line setting

--- src/tnc/main.py
+++ src/tnc/main.py
@@ -5,7 +5,7 @@ import os
 # Force qtpy to use PySide6
 os.environ['QT_API'] = 'pyside6'
 
-from PySide6.QtCore import Slot
+from PySide6.QtCore import QFileSystemWatcher, QTimer, Slot
 from PySide6.QtGui import QPalette
 from PySide6.QtWidgets import (QAbstractButton, QApplication)
 
@@ -28,6 +28,11 @@ DARK_STYLESHEET_FILE = "dark.qss"
 
 class MainWindow(VCPMainWindow):
     def __init__(self, *args, **kwargs):
+        opts = kwargs.get('opts')
+        self._develop_mode = bool(getattr(opts, 'develop', False))
+        self._qss_watcher = None
+        self._watched_stylesheet_path = None
+
         super(MainWindow, self).__init__(*args, **kwargs)
 
         self._connect_theme_tracking()
@@ -71,8 +76,33 @@ class MainWindow(VCPMainWindow):
         try:
             with open(stylesheet_path, 'r', encoding='utf-8') as style_file:
                 app.setStyleSheet(style_file.read())
+            if self._develop_mode:
+                self._watch_stylesheet(stylesheet_path)
         except Exception:
             LOG.exception("Failed to load theme stylesheet: %s", stylesheet_path)
+
+    def _watch_stylesheet(self, stylesheet_path):
+        if self._qss_watcher is None:
+            self._qss_watcher = QFileSystemWatcher(self)
+            self._qss_watcher.fileChanged.connect(self._on_stylesheet_file_changed)
+
+        if self._watched_stylesheet_path and self._watched_stylesheet_path != stylesheet_path:
+            self._qss_watcher.removePath(self._watched_stylesheet_path)
+
+        if stylesheet_path not in self._qss_watcher.files():
+            self._qss_watcher.addPath(stylesheet_path)
+
+        self._watched_stylesheet_path = stylesheet_path
+
+    def _on_stylesheet_file_changed(self, _path):
+        # QFileSystemWatcher may drop paths on save/rename, so reload then re-arm.
+        QTimer.singleShot(50, self._reload_watched_stylesheet)
+
+    def _reload_watched_stylesheet(self):
+        if self._watched_stylesheet_path and os.path.isfile(self._watched_stylesheet_path):
+            if self._watched_stylesheet_path not in self._qss_watcher.files():
+                self._qss_watcher.addPath(self._watched_stylesheet_path)
+        self._apply_theme_stylesheet()
 
 
     @Slot(QAbstractButton)
